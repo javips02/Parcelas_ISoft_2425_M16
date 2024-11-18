d9b5b86743b6272ebec987296eae5ff3252d5761
javips02 <javierperezsalasar@gmail.com>
2024-11-18 11:59:13 +0100

    Added javadoc

---
+++ docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"es.unizar.eina.parcelapad"},{"l":"es.unizar.eina.parcelapad.database.parcelas"},{"l":"es.unizar.eina.parcelapad.ui.parcelas"},{"l":"es.unizar.eina.reservapad.database.reservas"}];updateSearchResults();
