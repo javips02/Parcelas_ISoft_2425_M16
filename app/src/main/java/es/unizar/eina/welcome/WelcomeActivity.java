779e5cf2362dcd1d745d4995e51a3524066c67b0
javips02 <javierperezsalasar@gmail.com>
2024-12-02 12:21:13 +0100

    Integrada actividad de welcome con todo parcelapad

--- app/src/main/java/es/unizar/eina/welcome/WelcomeActivity.java
+++ app/src/main/java/es/unizar/eina/welcome/WelcomeActivity.java
@@ -1,15 +1,31 @@
 package es.unizar.eina.welcome;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import es.unizar.eina.notepad.R;
+import es.unizar.eina.parcelapad.ui.parcelas.Parcelapad;
 
 public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome); // Hace referencia al layout XML que creaste
+
+        // Obtén el botón de Parcelas
+        Button parcelasButton = findViewById(R.id.button_parcelas);
+
+        // Configura el listener para el botón
+        parcelasButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crea un Intent para lanzar la actividad Parcelapad
+                Intent intent = new Intent(WelcomeActivity.this, Parcelapad.class);
+                startActivity(intent);
+            }
+        });
     }
 }
-
