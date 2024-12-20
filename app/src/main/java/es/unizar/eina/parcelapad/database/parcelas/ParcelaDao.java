a445effd27e30176678f4ae30b4a879bc4e24a02
SergioGarces7 <843214@unizar.es>
2024-12-20 01:56:48 +0100

    Error raro apañado

--- app/src/main/java/es/unizar/eina/parcelapad/database/parcelas/ParcelaDao.java
+++ app/src/main/java/es/unizar/eina/parcelapad/database/parcelas/ParcelaDao.java
@@ -20,7 +20,7 @@ public interface ParcelaDao {
     @Update
     int update(Parcela parcela);
 
-    @Query("UPDATE parcela SET nombre = :nombre, desc = :desc, maxOcupantes = :maxOcupantes, precioParcela = :precio WHERE nombre = :originalNombre")
+    @Query("UPDATE parcela SET nombre = :nombre, `desc` = :desc, maxOcupantes = :maxOcupantes, precioParcela = :precio WHERE nombre = :originalNombre")
     int updateWithOriginalName(String originalNombre, String nombre, String desc, int maxOcupantes, double precio);
 
 
